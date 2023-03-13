4a3d744d318a8fb2669a7450a5f4032c586d3fe6
PPGamingDK <45538799+PPGamingDK@users.noreply.github.com>
2023-03-13 20:44:44 +0100

    Updated Cantines and added Details class

--- Entities/Cantines.cs
+++ Entities/Cantines.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,30 @@ using System.Threading.Tasks;
 
 namespace UIMock.Entities
 {
-    public class Cantines
+    public class Cantine
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-
         public string Price_Api { get; set; }
+        public Details Details { get; set; }
+
+        [JsonConstructor]
+        public Cantine(int id, string name, decimal price, string priceApi, string details)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Price_api = priceApi;
+            if (!string.IsNullOrEmpty(details))
+            {
+                var detailsDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(details);
+                Details = new Details(detailsDictionary["description"], detailsDictionary["openTime"],
+                                      detailsDictionary["closeTime"], detailsDictionary["menuDay1"],
+                                      detailsDictionary["menuDay2"], detailsDictionary["menuDay3"],
+                                      detailsDictionary["menuDay4"], detailsDictionary["menuDay5"],
+                                      detailsDictionary["menuDay6"], detailsDictionary["menuDay7"]);
+            }
+        }
     }
 }
---
+++ Entities/Details.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIMock.Entities
+{
+    public class Details
+    {
+        public string Description { get; set; }
+        public string OpenTime { get; set; }
+        public string CloseTime { get; set; }
+        public string MenuDay1 { get; set; }
+        public string MenuDay2 { get; set; }
+        public string MenuDay3 { get; set; }
+        public string MenuDay4 { get; set; }
+        public string MenuDay5 { get; set; }
+        public string MenuDay6 { get; set; }
+        public string MenuDay7 { get; set; }
+
+        public Details(string description, string openTime, string closeTime, string menuDay1, string menuDay2, string menuDay3, string menuDay4, string menuDay5, string menuDay6, string menuDay7)
+        {
+            Description = description;
+            OpenTime = openTime;
+            CloseTime = closeTime;
+            MenuDay1 = menuDay1;
+            MenuDay2 = menuDay2;
+            MenuDay3 = menuDay3;
+            MenuDay4 = menuDay4;
+            MenuDay5 = menuDay5;
+            MenuDay6 = menuDay6;
+            MenuDay7 = menuDay7;
+        }
+    }
+}
